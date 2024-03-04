7a972796b07b23d03989a260dae94ee084ed391d
Djenole da Silva <djenole@gmail.com>
2024-03-04 19:54:37 -0300

    Criado instância de Employee e EmployeeDTO

--- ems-backend/src/main/java/com/javaguides/ems/mapper/EmployeeMapper.java
+++ ems-backend/src/main/java/com/javaguides/ems/mapper/EmployeeMapper.java
@@ -1,2 +1,25 @@
-package com.javaguides.ems.mapper;public class EmployeeMapper {
+package com.javaguides.ems.mapper;
+
+import com.javaguides.ems.DTO.EmployeeDTO;
+import com.javaguides.ems.Entity.Employee;
+
+public class EmployeeMapper {
+    public static Employee mapToEmployeeDTO(Employee employee) {
+        return new Employee(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static EmployeeDTO mapToEmployee(EmployeeDTO employeeDTO) {
+        return new EmployeeDTO(
+                employeeDTO.getId(),
+                employeeDTO.getFirstName(),
+                employeeDTO.getLastName(),
+                employeeDTO.getEmail()
+        );
+    }
+
 }
