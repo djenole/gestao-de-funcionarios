377d473fbe4ce920acff87914457113743b62f6c
Djenole da Silva <djenole@gmail.com>
2024-03-04 19:47:28 -0300

    DTO de employee criado com anotação getter e setter do lombok

---
+++ ems-backend/src/main/java/com/javaguides/ems/DTO/EmployeeDTO.java
@@ -0,0 +1,17 @@
+package com.javaguides.ems.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeDTO {
+    private Long id;
+    private String firstName;
+    private String lateName;
+    private String email;
+}
