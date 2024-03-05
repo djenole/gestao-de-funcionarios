e04857ded30af0724e27ae8c10942c66eee4c9c8
Djenole da Silva <djenole@gmail.com>
2024-03-05 19:11:35 -0300

    controlador de Employee com anotação POST e endpoint

---
+++ ems-backend/src/main/java/com/javaguides/ems/Controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.javaguides.ems.Controller;
+
+import com.javaguides.ems.DTO.EmployeeDTO;
+import com.javaguides.ems.Entity.Employee;
+import com.javaguides.ems.Service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    @PostMapping
+    public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody EmployeeDTO employeeDTO) {
+        EmployeeDTO savedEmployee = employeeService.createEmployee(employeeDTO);
+
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+}
