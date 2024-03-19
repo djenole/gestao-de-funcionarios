478a72ff6429fbeb2810f1b7169d16973efac20f
Djenole da Silva <djenole@gmail.com>
2024-03-19 01:06:19 -0300

    implementações dos métodos listar, update e delete employee

--- ems-backend/src/main/java/com/javaguides/ems/Service/Impl/EmployeeServiceImpl.java
+++ ems-backend/src/main/java/com/javaguides/ems/Service/Impl/EmployeeServiceImpl.java
@@ -9,6 +9,9 @@ import com.javaguides.ems.mapper.EmployeeMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
@@ -24,10 +27,38 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
     @Override
     public EmployeeDTO getEmployeeId(Long employeeId) {
-       Employee employee = employeeRepository.findById(employeeId)
-        .orElseThrow(() -> new ResourceNotFoundException("Funcionário não encontrado por meio de ID"));
-    
+       Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+        () -> new ResourceNotFoundException("Funcionário não encontrado por meio de ID"));
     return EmployeeMapper.mapToEmployeeDTO(employee);
     }
-   
+
+    @Override
+    public List<EmployeeDTO> getAllEmployee() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDTO(employee))
+                .collect(Collectors.toList());
+
+    }
+    @Override
+    public EmployeeDTO updateEmployee(Long employeeId, EmployeeDTO updatedEmployee) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+            () -> new ResourceNotFoundException("Funcionário não encontrado por meio de ID: " + employeeId));
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmail(updatedEmployee.getEmail());
+        
+        Employee updatedEmployeeObject = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDTO(updatedEmployeeObject);
+    }
+
+    @Override
+    public void deleteEmployee(Long employeeId) {
+    Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new ResourceNotFoundException("Funcionário não encontrado por meio de ID:" + employeeId));
+    employeeRepository.deleteById(employeeId);
+
+
+    }
+
+
 }
