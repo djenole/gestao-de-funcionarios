a48c3cc3e81a1d0b2ed127735aa8e1e0065dbb21
Djenole da Silva <djenole@gmail.com>
2024-03-18 22:31:37 -0300

    Controller que retorna de um ResponseEntity um Employee ID

--- ems-backend/src/main/java/com/javaguides/ems/Controller/EmployeeController.java
+++ ems-backend/src/main/java/com/javaguides/ems/Controller/EmployeeController.java
@@ -1,11 +1,12 @@
 package com.javaguides.ems.Controller;
 
 import com.javaguides.ems.DTO.EmployeeDTO;
-import com.javaguides.ems.Entity.Employee;
 import com.javaguides.ems.Service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
     private EmployeeService employeeService;
 
+    //Add employee REST API
     @PostMapping
     public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody EmployeeDTO employeeDTO) {
         EmployeeDTO savedEmployee = employeeService.createEmployee(employeeDTO);
 
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
+
+    //Get employee Rest API
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable("id") Long employeeId) {
+        EmployeeDTO getEmployeeID = employeeService.getEmployeeId(employeeId);
+
+        return ResponseEntity.ok(getEmployeeID);
+    }
+
+
 }
