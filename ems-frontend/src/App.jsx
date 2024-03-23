719cd9ad34c0aa728a6d005cb225f4ce6a03a2d6
Djenole da Silva <djenole@gmail.com>
2024-03-23 07:57:31 -0300

    componente header e footer importados e usados no App component

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,14 +1,18 @@
 
 import './App.css'
+import { FooterComponent } from './Components/FooterComponent'
+import { HeaderComponent } from './Components/HeaderComponent'
 import ListEmployeeComponent from './Components/ListEmployeeComponent'
 
 function App() {
 
 
   return (
-    <>
+    <div>
+      <HeaderComponent/>
       <ListEmployeeComponent />
-    </>
+      <FooterComponent />
+    </div>
   )
 }
 
