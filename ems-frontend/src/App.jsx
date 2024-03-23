77771ca8b9e186a62173e507e1f3cb210e5ac1e6
Djenole da Silva <djenole@gmail.com>
2024-03-23 09:15:01 -0300

    adicionado caminho para add-employee com o elemento de EmployeeComponent

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,17 +1,27 @@
 
 import './App.css'
+import { EmployeeComponent } from './Components/EmployeeComponent'
 import { FooterComponent } from './Components/FooterComponent'
 import { HeaderComponent } from './Components/HeaderComponent'
 import ListEmployeeComponent from './Components/ListEmployeeComponent'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 function App() {
 
 
   return (
     <div>
-      <HeaderComponent/>
-      <ListEmployeeComponent />
-      <FooterComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+          <Routes>
+            <Route path='/' element={<ListEmployeeComponent/>}/>
+            <Route path='/employees' element={<ListEmployeeComponent/>}/>
+            <Route path='/add-employee' element={<EmployeeComponent/>} />
+          </Routes>
+
+        <FooterComponent />
+
+      </BrowserRouter>
+
     </div>
   )
 }
