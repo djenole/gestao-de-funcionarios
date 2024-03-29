44fb907e228139d1e6d76c559230934702533037
Djenole da Silva <djenole@gmail.com>
2024-03-28 21:42:55 -0300

    criado entidade de Department com anotações lombok

---
+++ ems-backend/src/main/java/com/javaguides/ems/Entity/Department.java
@@ -0,0 +1,29 @@
+package com.javaguides.ems.Entity;
+
+import com.javaguides.ems.DTO.DepartmentDTO;
+import com.javaguides.ems.DTO.EmployeeDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "department_name")
+    private String departmentName;
+
+    @Column(name = "department_description")
+    private String departmentDescription;
+
+}
