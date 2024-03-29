4a7b5b5a70c5d12c6f3cb1163ea321eadbdc69e2
Djenole da Silva <djenole@gmail.com>
2024-03-29 01:58:04 -0300

    adicionado rotas para listagem, add e edit

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,8 +1,10 @@
 
 import './App.css'
+import { DepartmentComponent } from './Components/DepartmentComponent'
 import { EmployeeComponent } from './Components/EmployeeComponent'
 import { FooterComponent } from './Components/FooterComponent'
 import { HeaderComponent } from './Components/HeaderComponent'
+import ListDepartmentComponent from './Components/ListDepartmentComponent'
 import ListEmployeeComponent from './Components/ListEmployeeComponent'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 function App() {
@@ -17,6 +19,10 @@ function App() {
             <Route path='/employees' element={<ListEmployeeComponent/>}/>
             <Route path='/add-employee' element={<EmployeeComponent/>} />
             <Route path='/edit-employee/:id' element={<EmployeeComponent />}/>
+
+            <Route path='/departments' element={<ListDepartmentComponent/>}/>
+            <Route path='/add-department/' element={<DepartmentComponent/>}/>
+            <Route path='edit-department/:id' element={<DepartmentComponent/>} />
           </Routes>
 
         <FooterComponent />
